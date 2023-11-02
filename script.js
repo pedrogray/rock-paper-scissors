a386ee78be130168f2edb37dc03c25c1925d2285
Pedro Gray <144771627+pedrogray@users.noreply.github.com>
2023-11-02 12:01:47 -0700

    Add PlayRound function to play out a single round of RPS. Add game function which loops PlayRound up to 5 times unless player or computer wins 3 times.

--- script.js
+++ script.js
@@ -16,5 +16,30 @@ function getPlayerChoice() {
 function playRound() {
     let computer = getComputerChoice();
     let player = getPlayerChoice();
-    
+    if (computer == 'scissors' && player == 'rock') {
+        console.log("Player wins! Rock beats scissors!")
+        return playerWins++}
+    else if (computer == 'scissors' && player =='paper') {
+        console.log("Computer wins! Scissors beat paper.")
+        return computerWins++}
+    else if (computer == 'paper' && player == 'rock') {
+        console.log('Computer wins! Paper beats rock.')
+        return computerWins++}
+    else if (computer == 'paper' && player == 'scissors') {
+        console.log('Player wins! Scissors beat paper.')
+        return playerWins++}
+    else if (computer = player) {
+        console.log("It's a tie!")
+        return}
+}
+
+function game() {
+    playerWins = 0
+    computerWins = 0
+    for (let i=1; i<=5; i++) {
+        playRound()
+        if (computerWins == 3 ) {return "Game Over! Computer wins!"}
+        else if (playerWins == 3 ) {return ("Game over! Player Wins!")}
+        else if (i==5) {return "It's a tie!"}
+    }
 }
